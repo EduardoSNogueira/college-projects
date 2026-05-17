87408adbb520fcd4adbbff39d39f874b768ae159
EduardoSNogueira <edu.esn@hotmail.com>
2026-05-16 21:37:55 -0300

    feat: implement initial CLI menu structure with match-case

---
+++ library-system/main.py
@@ -0,0 +1,35 @@
+
+def menu():
+    while True:
+        print("\n   SISTEMA DE EMPRESTIMO BIBLIOTECA   ")
+        print("1. Cadastrar livro")
+        print("2. Cadastrar usuário")
+        print("3. Consultar livros")
+        print("4. Emprestar livro")
+        print("5. Devolver Livro")
+        print("6. Gerar relatório")
+        print("7. Salvar")
+        print("8. SAIR")
+
+        opcao = input("").strip()
+
+        match opcao:
+            case "1":
+                cadastrarLivro(livros)
+            case "2":
+                cadastrarUsuario(aluno)
+            case "3":
+                consultarLivros(livros)
+            case "4":
+                emprestarLivro(livros, aluno)
+            case "5":
+                devolverLivro(livros)
+            case "6":
+                relatorio(livros, aluno)
+            case "7":
+                salvarCsv(livros, aluno)
+            case "8":
+                print("Saindo... ate logo")
+                break
+            case _:
+                print("ERRO: Opção Invalida")
